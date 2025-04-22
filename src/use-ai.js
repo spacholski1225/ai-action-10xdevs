93aefc8b19ff5053e5ce683cd5bf4007d97e9613
psmyrdek <przemek.smyrdek@gmail.com>
2025-04-22 14:14:20 +0200

    chore: bump

--- src/use-ai.js
+++ src/use-ai.js
@@ -7,7 +7,7 @@ const ai = new GoogleGenAI({apiKey: process.env.GOOGLE_API_KEY});
 
 async function main() {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash-preview-04-17",
+    model: "gemini-2.0-flash",
     contents:
       "Provide short example of a feedback to Pull Request - imagine that you are a reviewer.",
   });
